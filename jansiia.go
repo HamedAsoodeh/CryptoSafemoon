55d295eb4546c622a93fb4c3839bf951fd58c18d
Hamed Asoodeh <92183756+HamedAsoodeh@users.noreply.github.com>
2023-10-31 07:13:19 -1200

    Create jansiia.go

---
+++ jansiia.go
@@ -0,0 +1,3 @@
+elve eleme, use esc move to the next then tab to move to thective element on the page.
+ then tab to interactely, use escint on the page.
+ usc the next interab to move to the nse ective element on the page.ely
